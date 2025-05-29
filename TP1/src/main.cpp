fe5e92af2bdd415fa4e900fad3efa4a471953af8
MarceleLouiseSAra <louise20marcele03@gmail.com>
2025-05-29 17:22:26 -0300

    feat: heap.hpp e Makefile

---
+++ TP1/src/main.cpp
@@ -0,0 +1,14 @@
+#include "node.hpp"
+#include "listaEncadeada.hpp"
+#include "pilha.hpp"
+#include "grafo.hpp"
+#include "heap.hpp"
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+int main () {
+    return 0;
+}
